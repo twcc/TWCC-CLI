35d81e70b0a0d983eab31d68d6a94f5192a2659b
fychao <1803001@narlabs.org.tw>
2018-11-05 10:34:21 +0800

    add api v5.1.1 yaml and diff tool

---
+++ src/tools/swagger_diff.py
@@ -0,0 +1,69 @@
+from __future__ import print_function
+from os import sys, path, strerror
+import argparse
+import yaml
+import errno
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+
+def doAnaly(fn_old = "../yaml/api_v2_swagger_v5.0.0_staging.yaml",
+        fn_new = "../yaml/api_v2_swagger_v5.1.1_staging.yaml" ):
+
+    swg_old = yaml.load(open(fn_old, 'r').read())
+    swg_new = yaml.load(open(fn_new, 'r').read())
+
+    swg_old_fns = set(swg_old['paths'].keys())
+    swg_new_fns = set(swg_new['paths'].keys())
+
+    print ("==="*5, "Old diff New", "==="*5)
+    odn =  swg_old_fns.difference( swg_new_fns )
+    if len(odn)==0:
+        print (">>>> no change <<<<<");
+    else:
+        print ("API URL |\t HTTP Verb |\t Summary |");
+        print ("| -- | -- | -- | ")
+        for x in odn:
+            for y in swg_old['paths'][x]:
+                print ("|", " |\t".join( [x, y, swg_old['paths'][x][y]['summary']]))
+    print ()
+    print ("==="*5, "New diff Old", "==="*5)
+    ndo = swg_new_fns.difference( swg_old_fns )
+    if len(ndo)==0:
+        print (">>>> no change <<<<<");
+    else:
+        print ("API URL |\t HTTP Verb |\t Summary |");
+        print ("| -- | -- | -- | ")
+        for x in ndo:
+            for y in swg_new['paths'][x]:
+                print ("|", " |\t".join( [x, y, swg_new['paths'][x][y]['summary']]))
+
+
+if __name__  ==  '__main__':
+	parser = argparse.ArgumentParser(description='Show difference between Old and New SWAGGER documents')
+        parser.add_argument('-p', "--path_yaml",
+                default="../yaml",
+                help='where yaml file store, (default: %(default)s)')
+
+        group = parser.add_argument_group()
+        group.add_argument('-n', "--new_yaml",
+                default="api_v2_swagger_v5.0.0_staging.yaml",
+                help='latest yaml file name, (default: %(default)s)')
+        group.add_argument('-o', "--old_yaml",
+                default="api_v2_swagger_v5.1.1_staging.yaml",
+                help='previous yaml file name, (default: %(default)s)')
+
+	args = parser.parse_args()
+
+
+        p_yaml = args.path_yaml
+        n_yaml = "{0}/{1}".format(p_yaml, args.new_yaml)
+        o_yaml = "{0}/{1}".format(p_yaml, args.old_yaml)
+
+        if not path.isfile( o_yaml ):
+            raise OSError, "{0} file not found".format(o_yaml)
+        if not path.isfile( n_yaml ):
+            raise OSError, "{0} file not found".format(n_yaml)
+
+        doAnaly( fn_old = n_yaml, fn_new = o_yaml )
+
