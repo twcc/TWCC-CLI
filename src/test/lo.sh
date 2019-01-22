de57e1ded4e7ce20660e6311bf3ff7f21d88ab39
Gunter <williamchen.nchc@gmail.com>
2019-01-22 14:22:10 +0800

    Update Network

--- src/test/lo.sh
+++ src/test/lo.sh
@@ -1,1 +1,2 @@
-sshpass -p Z9K?3{e6Z9K ssh littledd18@203.145.219.128 -p 56336 -o StrictHostKeyChecking=no -L 5000:127.0.0.1:5001
+#sshpass -p Z9K?3{e6Z9K ssh littledd18@203.145.219.128 -f -N -p 56336 -o StrictHostKeyChecking=no 'cd ai-benchmark/;touch hello.txt'
+sshpass -p Z9K?3{e6Z9K ssh littledd18@203.145.219.128 -p 56336 -o StrictHostKeyChecking=no
--- src/test/login.sh
+++ src/test/login.sh
@@ -1,1 +1,3 @@
-sshpass -p Z9K?3{e6Z9K ssh littledd18@203.145.219.128 -L 5001:127.0.0.1:5001 -p 56336 -o StrictHostKeyChecking=no 'cd ai-benchmark/;python v3_web.py' &
+sshpass -p 'Z9K?3{e6Z9K' ssh littledd18@203.145.219.128 -f -L 5001:127.0.0.1:5001 -p 56336 -o StrictHostKeyChecking=no 'cd ai-benchmark/;python v3_web.py' &
+#sshpass -p 'Z9K?3{e6Z9K' ssh littledd18@203.145.219.128 -v -L 5001:127.0.0.1:5001 -p 56336 -o StrictHostKeyChecking=no
+echo "Hi!!"
