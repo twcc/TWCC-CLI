34abc89ecf91d67b753ad4e532edf2077a16d94a
fychao <1803001@narlabs.org.tw>
2020-01-31 16:21:17 +0800

    del global variables and add ignore

--- src/twcc/session.py
+++ src/twcc/session.py
@@ -201,6 +201,5 @@ def session_start():
         TWCC_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
         return Session( twcc_yaml_path="{}/yaml/NCHC_API-Test_env.yaml".format(TWCC_PATH) )
     else:
-        global _TWCC_SESSION_
         return _TWCC_SESSION_
 
