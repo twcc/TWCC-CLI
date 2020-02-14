f265b42773064d2c746f83fa83f8c997b9d00cdc
August F.Y. Chao <AugustChao@narlabs.org.tw>
2020-02-14 10:29:59 +0800

    chagnes setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 import os
 from setuptools import setup, find_packages
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,12 +25,15 @@ def find_version(*file_paths):
         return version_match.group(1)
     raise RuntimeError("Unable to find version string.")
 
+install_reqs = parse_requirements('requirements.txt', session=PipSession())
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name='twccli',
     version='0.2',
     py_modules=['twccli'],
     packages=find_packages(),
+    install_requires= reqs,
     license="Apache License 2.0",
     entry_points='''
         [console_scripts]
