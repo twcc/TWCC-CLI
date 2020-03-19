9bd737cbc36f6df801a205e1fdbeb546ee1d99df
fychao <1803001@narlabs.org.tw>
2020-03-19 13:32:27 +0800

    shop v0.4.8

--- twccli/version.py
+++ twccli/version.py
@@ -1,5 +1,5 @@
 __TWCC_CLI_MAJOR_ = "0"
 __TWCC_CLI_MINOR_ = "4"
-__TWCC_CLI_BATCH_ = "7"
+__TWCC_CLI_BATCH_ = "8"
 __version__ = "{}.{}.{}".format(
     __TWCC_CLI_MAJOR_, __TWCC_CLI_MINOR_, __TWCC_CLI_BATCH_)
