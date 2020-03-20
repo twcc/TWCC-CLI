c35ad6c29fdee4ef2b4f136834a8f1a041141b0d
NCHC_August Chao <1803001@narlabs.org.tw>
2020-03-20 17:26:33 +0800

    Update version.py

--- twccli/version.py
+++ twccli/version.py
@@ -1,5 +1,5 @@
 __TWCC_CLI_MAJOR_ = "0"
 __TWCC_CLI_MINOR_ = "5"
-__TWCC_CLI_BATCH_ = "1"
+__TWCC_CLI_BATCH_ = "1a"
 __version__ = "{}.{}.{}".format(
     __TWCC_CLI_MAJOR_, __TWCC_CLI_MINOR_, __TWCC_CLI_BATCH_)
