5543e90ea43509da6782dccd78b7139bea6d327f
fychao <1803001@narlabs.org.tw>
2020-03-20 17:45:56 +0800

    fix version.py error

--- twccli/version.py
+++ twccli/version.py
@@ -1,5 +1,5 @@
 __TWCC_CLI_MAJOR_ = "0"
 __TWCC_CLI_MINOR_ = "5"
 __TWCC_CLI_BATCH_ = "2"
-__version__ = "{}.{}.{}.{}".format(
+__version__ = "{}.{}.{}".format(
     __TWCC_CLI_MAJOR_, __TWCC_CLI_MINOR_, __TWCC_CLI_BATCH_)
