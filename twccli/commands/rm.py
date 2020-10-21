fbcc6a3ae6aa3c588d686ebe419a31b168e38d0d
Leo Chen <2003051@nchc.narl.org.tw>
2020-10-21 14:02:36 +0800

    fix " bug...

--- twccli/commands/rm.py
+++ twccli/commands/rm.py
@@ -197,7 +197,7 @@ def key(ctx, name, ids_or_names, force):
 @click.option('-key', '--keypair', 'res_property', flag_value='Keypair',
               help="Delete existing keypair(s).")
 @click.option('-snap', '--snapshots', 'res_property', flag_value='Snapshot',
-              help="delete snapshots. `-snap-id` is required!"")
+              help="delete snapshots. `-snap-id` is required!")
 @click.option('-secg', '--security-group', 'res_property', flag_value='SecurityGroup',
               help="Delete existing security group(s).")
 @click.argument('ids_or_names', nargs=-1)
