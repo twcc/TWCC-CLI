98b6fdffb7d3149dfac53935ddff7c52050264ba
fychao <1803001@narlabs.org.tw>
2020-11-05 17:50:39 +0800

    chg v2 to v3 for vcs site

--- twccli/twcc/clidriver.py
+++ twccli/twcc/clidriver.py
@@ -33,15 +33,18 @@ class ServiceOperation:
         self.host_url = Session2._getTwccApiHost()
 
         _ava_funcs_ = self.twcc_config['avalible_funcs']
-        self.valid_funcs = [_ava_funcs_[x]['name']
-                            for x in range(len(_ava_funcs_))]
+        self.valid_funcs = [
+            _ava_funcs_[x]['name'] for x in range(len(_ava_funcs_))
+        ]
 
-        self.valid_http_verb = dict([(_ava_funcs_[x]['name'], _ava_funcs_[x][
-                                    'http_verb']) for x in range(len(_ava_funcs_))])
+        self.valid_http_verb = dict([(_ava_funcs_[x]['name'],
+                                      _ava_funcs_[x]['http_verb'])
+                                     for x in range(len(_ava_funcs_))])
         self.url_format = dict([(_ava_funcs_[x]['name'],
-                                 _ava_funcs_[x]['url_type']) for x in range(len(_ava_funcs_))])
-        self.url_ptn = dict([
-            (x, parsePtn(self.url_format[x])) for x in self.url_format.keys()])
+                                 _ava_funcs_[x]['url_type'])
+                                for x in range(len(_ava_funcs_))])
+        self.url_ptn = dict([(x, parsePtn(self.url_format[x]))
+                             for x in self.url_format.keys()])
 
     def load_credential(self):
         # @todo
@@ -66,14 +69,17 @@ class ServiceOperation:
 
         _ava_funcs_ = twcc_conf['avalible_funcs']
 
-        self.valid_funcs = [_ava_funcs_[x]['name']
-                            for x in range(len(_ava_funcs_))]
-        self.valid_http_verb = dict([(_ava_funcs_[x]['name'], _ava_funcs_[x][
-                                    'http_verb']) for x in range(len(_ava_funcs_))])
+        self.valid_funcs = [
+            _ava_funcs_[x]['name'] for x in range(len(_ava_funcs_))
+        ]
+        self.valid_http_verb = dict([(_ava_funcs_[x]['name'],
+                                      _ava_funcs_[x]['http_verb'])
+                                     for x in range(len(_ava_funcs_))])
         self.url_format = dict([(_ava_funcs_[x]['name'],
-                                 _ava_funcs_[x]['url_type']) for x in range(len(_ava_funcs_))])
-        self.url_ptn = dict([
-            (x, parsePtn(self.url_format[x])) for x in self.url_format.keys()])
+                                 _ava_funcs_[x]['url_type'])
+                                for x in range(len(_ava_funcs_))])
+        self.url_ptn = dict([(x, parsePtn(self.url_format[x]))
+                             for x in self.url_format.keys()])
 
         self.twcc_conf = twcc_conf
 
@@ -87,11 +93,23 @@ class ServiceOperation:
 
         if isNone(t_data):
             command = "curl -s -X {method} \\\n -H {headers} \\\n '{uri}'"
-            return command.format(method=mtype.upper(), headers=headers, data=t_data, uri=t_api)
+            return command.format(method=mtype.upper(),
+                                  headers=headers,
+                                  data=t_data,
+                                  uri=t_api)
         else:
             command = "curl -s -X {method} \\\n -H {headers} \\\n -d '{data}' \\\n '{uri}'"
-            return command.format(method=mtype.upper(), headers=headers, data=t_data, uri=t_api)
-    def _api_act(self, t_api, t_headers, t_data=None, mtype="get", show_curl=False):
+            return command.format(method=mtype.upper(),
+                                  headers=headers,
+                                  data=t_data,
+                                  uri=t_api)
+
+    def _api_act(self,
+                 t_api,
+                 t_headers,
+                 t_data=None,
+                 mtype="get",
+                 show_curl=False):
 
         if show_curl:
             self._to_curl(t_api, t_headers, t_data, mtype)
@@ -102,16 +120,22 @@ class ServiceOperation:
             r = requests.get(t_api, headers=t_headers, verify=False)
 
         elif mtype == 'post':
-            r = requests.post(t_api, headers=t_headers,
-                              data=json.dumps(t_data), verify=False)
+            r = requests.post(t_api,
+                              headers=t_headers,
+                              data=json.dumps(t_data),
+                              verify=False)
         elif mtype == "delete":
             r = requests.delete(t_api, headers=t_headers, verify=False)
         elif mtype == "patch":
-            r = requests.patch(t_api, headers=t_headers,
-                              data=json.dumps(t_data), verify=False)
+            r = requests.patch(t_api,
+                               headers=t_headers,
+                               data=json.dumps(t_data),
+                               verify=False)
         elif mtype == "put":
-            r = requests.put(t_api, headers=t_headers,
-                             data=json.dumps(t_data), verify=False)
+            r = requests.put(t_api,
+                             headers=t_headers,
+                             data=json.dumps(t_data),
+                             verify=False)
         else:
             raise ValueError("http verb:'{0}' is not valid".format(mtype))
 
@@ -122,20 +146,23 @@ class ServiceOperation:
                     (t_api, r.status_code, time.time() - start_time))
         return (r, (time.time() - start_time))
 
-    def doAPI(
-        self,
-            site_sn=None,
-            api_host=None,
-            key_tag=None, api_key=None,
-            ctype="application/json",
-            func=None,
-            url_dict=None, data_dict=None, url_ext_get=None,
-            http='get', res_type='json'):
+    def doAPI(self,
+              site_sn=None,
+              api_host=None,
+              key_tag=None,
+              api_key=None,
+              ctype="application/json",
+              func=None,
+              url_dict=None,
+              data_dict=None,
+              url_ext_get=None,
+              http='get',
+              res_type='json'):
 
         if not res_type in self.res_type_valid:
             raise ValueError(
-                "Response type Error:'{0}' is not valid, available options: {1}".format(
-                    res_type, ", ".join(self.res_type_valid)))
+                "Response type Error:'{0}' is not valid, available options: {1}"
+                .format(res_type, ", ".join(self.res_type_valid)))
 
         if not self.isFunValid(func):
             raise ValueError("Function for:'{0}' is not valid".format(func))
@@ -143,15 +170,18 @@ class ServiceOperation:
             raise ValueError("http verb:'{0}' is not valid".format(http))
 
         t_url = self.mkAPIUrl(site_sn, api_host, func, url_dict=url_dict)
-        t_header = self.mkHeader(
-            site_sn=site_sn, key_tag=key_tag, api_host=api_host, api_key=api_key, ctype=ctype)
+        t_header = self.mkHeader(site_sn=site_sn,
+                                 key_tag=key_tag,
+                                 api_host=api_host,
+                                 api_key=api_key,
+                                 ctype=ctype)
 
         if not isNone(url_ext_get):
             t_url += "?"
             t_url_tmp = []
             for param_key in url_ext_get.keys():
-                t_url_tmp.append("{0}={1}".format(
-                    param_key, url_ext_get[param_key]))
+                t_url_tmp.append("{0}={1}".format(param_key,
+                                                  url_ext_get[param_key]))
             t_url += "&".join(t_url_tmp)
 
         res = self._api_act(t_url, t_header, t_data=data_dict, mtype=http)
@@ -164,8 +194,11 @@ class ServiceOperation:
             elif res_type == 'txt':
                 return res[0].content
 
-    def mkHeader(self, site_sn=None, key_tag=None,
-                 api_host=None, api_key=None,
+    def mkHeader(self,
+                 site_sn=None,
+                 key_tag=None,
+                 api_host=None,
+                 api_key=None,
                  ctype="application/json"):
 
         self.api_host = api_host
@@ -174,10 +207,11 @@ class ServiceOperation:
 
         from twccli.version import __version__
         return_header = {
-                         'User-Agent': 'TWCC-CLI v%s'%(__version__),
-                         'X-API-HOST': site_sn,
-                         'x-api-key': api_key,
-                         'Content-Type': self.ctype}
+            'User-Agent': 'TWCC-CLI v%s' % (__version__),
+            'X-API-HOST': site_sn,
+            'x-api-key': api_key,
+            'Content-Type': self.ctype
+        }
 
         if len(self.header_extra.keys()) > 0:
             for key in self.header_extra.keys():
@@ -218,9 +252,7 @@ class ServiceOperation:
         self._i("-" * 10 + "=" * 10 + " [info] BEGIN " + "=" * 10 + "-" * 10)
         self._i("-" * 10 + "=" * 10 + " [info] ENDS  " + "=" * 10 + "-" * 10)
 
-    def mkAPIUrl(self,
-                 site_sn=None, api_host=None,
-                 func=None, url_dict=None):
+    def mkAPIUrl(self, site_sn=None, api_host=None, func=None, url_dict=None):
 
         # check if this function valid
         if not self.isFunValid(func):
@@ -247,8 +279,8 @@ class ServiceOperation:
                 ptn = "%s/%s" % (func, url_dict[func])
                 del url_dict[func]
 
-                ptn += "/"+"/".join(["%s/%s" % (k, url_dict[k])
-                                     for k in url_dict.keys()])
+                ptn += "/" + "/".join(
+                    ["%s/%s" % (k, url_dict[k]) for k in url_dict.keys()])
 
                 # todos
                 ptn = ptn.strip("/")
@@ -262,5 +294,9 @@ class ServiceOperation:
         t_url = url_str
         for ptn in url_parts.keys():
             t_url = t_url.replace(url_ptn[ptn], url_parts[ptn])
-
+        # need to migrate /v3/
+        if url_parts[
+                'PLATFORM'] == "openstack-taichung-default-2" and url_parts[
+                    'FUNCTION'] == 'sites':
+            t_url = t_url.replace("/v2/", "/v3/")
         return self.host_url + t_url
