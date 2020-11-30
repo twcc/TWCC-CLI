065f43c173b16cfb5bd14fb21d93584cf0023052
Leo <2003051@nchc.narl.org.tw>
2020-11-30 15:47:47 +0800

    set logdir

--- twccli/twccli.py
+++ twccli/twccli.py
@@ -7,11 +7,14 @@ plugin_folder = os.path.join(os.path.dirname(__file__), 'commands')
 os.environ['LANG'] = 'C.UTF-8'
 os.environ['LC_ALL'] = 'C.UTF-8'
 
+if "TWCC_DATA_PATH" in os.environ and os.path.isdir(os.environ['TWCC_DATA_PATH']):
+    log_dir = "{}/log".format(os.environ['TWCC_DATA_PATH'])
+else:
+    log_dir = "{}/log".format(os.path.join(os.environ['HOME'], '.twcc_data'))
 
 if sys.version_info[0] == 3 and sys.version_info[1] >= 5:
     from loguru import logger
     logger.remove()
-    log_dir = "{}/log".format(os.environ['TWCC_DATA_PATH'])
     logger.add(sys.stderr, level="DEBUG")
     logger.add(os.path.join(log_dir,"twcc.log"), format="{time:YYYY-MM-DD HH:mm:ss} |【{level}】| {file} {function} {line} | {message}",
             rotation="00:00", retention='20 days', encoding='utf8', level="INFO", mode='a')
@@ -22,7 +25,6 @@ else:
     import logging.config
     with open('twccli/logging.yml', 'r') as f:
         config = yaml.load(f,Loader=yaml.FullLoader)
-    log_dir = "{}/log".format(os.environ['TWCC_DATA_PATH'])
     log_file_name = config['handlers']['file']['filename']
     config['handlers']['file']['filename'] = os.path.join(log_dir,log_file_name)
     logging.config.dictConfig(config)
