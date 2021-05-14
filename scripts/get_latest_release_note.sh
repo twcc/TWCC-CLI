146e4dddc5a61ad914f2b0d4c10b603b066cd215
AugChao <august.chao@twsc.io>
2021-05-14 02:40:25 +0000

    change script

--- scripts/get_latest_release_note.sh
+++ scripts/get_latest_release_note.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 CMD=`grep -hnr "### v" README.md | sed 's/:### v/ /' | awk '/ /{print $1}' | head -n 2 |paste -sd " " -  | awk '/ /{print $1-1 " " $2}'`
-python -c "import sys; begin=int(sys.argv[1]); end=int(sys.argv[2]); stm=open('README.md', 'r').readlines(); print('\\n'.join([ x.strip() for x in stm[begin:end]]))" $CMD
+python -c "import sys; begin=int(sys.argv[1]); end=int(sys.argv[2]); stm=open('README.md', 'r').readlines(); import json; print(json.dumps(''.join([ x.strip() for x in stm[begin:end]])))" $CMD
