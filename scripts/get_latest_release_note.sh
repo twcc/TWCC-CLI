ba8162f4ae901c1f3cfc8af6244eb5e850334d1a
AugChao <august.chao@twsc.io>
2021-05-14 02:33:29 +0000

    test cicd

--- scripts/get_latest_release_note.sh
+++ scripts/get_latest_release_note.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CMD=`grep -hnr "### v" README.md | sed 's/:### v/ /' | awk '/ /{print $1}' | head -n 2 |paste -sd " " -  | awk '/ /{print "cat README.md | head -n " $2-1  " | tail  -" $2-$1}'`
-eval $CMD
+CMD=`grep -hnr "### v" README.md | sed 's/:### v/ /' | awk '/ /{print $1}' | head -n 2 |paste -sd " " -  | awk '/ /{print $1-1 " " $2}'`
+python -c "import sys; begin=int(sys.argv[1]); end=int(sys.argv[2]); stm=open('README.md', 'r').readlines(); print('\\n'.join([ x.strip() for x in stm[begin:end]]))" $CMD
