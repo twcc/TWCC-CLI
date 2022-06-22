c8b1e2dc9ed25ac9295a4a6308f72aa7699a1127
Leo Chen <leo.chen@twsc.io>
2022-06-22 16:35:44 +0800

    fix export LANG on centos7.9

--- twccli/commands/config.py
+++ twccli/commands/config.py
@@ -15,6 +15,12 @@ export LC_ALL=C.UTF-8
 export PYTHONIOENCODING=UTF-8
 """
 
+lang_encoding_centos79 = """
+export LANG=zh_TW.utf-8
+export LC_ALL=zh_TW.utf-8
+export PYTHONIOENCODING=UTF-8
+"""
+
 
 @click.command(help='Get exsisting information.')
 # @click.option("-v", "--verbose", is_flag=True, help="Enable verbose mode.")
@@ -96,10 +102,11 @@ def init(env, apikey, proj_code, rc, user_agent, ga_flag):
 
             click.echo(click.style("Hi! {}, welcome to TWCC!".format(
                 Session2._whoami()['display_name']), fg='yellow'))
-
+            import platform
+            if platform.linux_distribution()[0] == 'CentOS Linux' and platform.linux_distribution()[1][:3] == '7.9':
+                lang_encoding = lang_encoding_centos79
             if rc:
                 click.echo("Add language setting to `.bashrc`.")
-
                 open(os.environ["HOME"]+"/.bashrc", 'a').write(lang_encoding)
             else:
                 click.echo(
