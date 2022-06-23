5d0e3549dface90a64a9868c5719e91b3c40d9cc
August Chao <august.chao@twsc.io>
2022-06-23 13:03:37 +0800

    change to bucket policy

--- scripts/cos-permission-sharing/run.py
+++ scripts/cos-permission-sharing/run.py
@@ -1,4 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import boto3
+import json
 
 s3_keys = {
     'user1': {  # publc cos key
@@ -13,8 +17,8 @@ s3_keys = {
 }
 
 whom = 'user1'
-testing_bkt = 'zzzzz_aug_cli'
-testing_okey = 'README.md'
+testing_bkt = 'cus01132'
+testing_okey = 'thanks.gif'
 
 
 class cos_client:
@@ -40,45 +44,46 @@ class cos_client:
         if bucket != "":
             return [x['Key'] for x in self.client.list_objects_v2(Bucket=bucket)['Contents']]
 
-    def get_bucket_acl(self, bucket=""):
-        return self.client.get_bucket_acl(Bucket=bucket)
+    def get_bucket_policy(self, bucket=""):
+        try:
+            res = self.client.get_bucket_policy(Bucket=bucket)
+            return res
+        except Exception as e:
+            print(e)
 
     def share_readonly_to(self, uuid="", bucket=""):
         """
-        put object acl, https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html?highlight=put_object_acl#S3.Client.put_object_acl
-
-        permission details https://docs.aws.amazon.com/AmazonS3/latest/userguide/acl-overview.html#CannedACL
-        selected acl:
-        - authenticated-read
-        - public-read
+        https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-bucket-policies.html
         """
         if uuid != "" and bucket != "":
-            existing_ids = self._parse_id_in_acl(
-                self.get_bucket_acl(bucket)['Grants'])
+            bucket_policy = {
+                'Version': '2012-10-17',
+                'Statement': [{
+                    'Sid': 'AddROPerm',
+                    'Effect': 'Allow',
+                    'Action': [
+                        's3:GetObject',
+                        's3:GetObjectVersion',
+                        's3:ListBucket'],
+                    'Resource': [
+                        f'arn:aws:s3:::{bucket}/*',
+                        f'arn:aws:s3:::{bucket}',
+                    ],
+                    "Principal": {
+                        "AWS": [ "arn:aws:iam:::user/%s"%(uuid) ]
+                     },
+                }]
+            }
+            self.client.put_bucket_policy(Bucket=bucket, Policy=json.dumps(bucket_policy))
 
-            if not uuid in existing_ids:
-                existing_ids.append(uuid)
-            grant_read = ",".join(["id=\"%s\"" % x for x in existing_ids])
-            self.client.put_bucket_acl(
-                Bucket=testing_bkt,
-                GrantRead=grant_read,
-            )
 
-    def _parse_id_in_acl(self, grants):
-        return [x['Grantee']['ID'] for x in grants]
+    def get_obj_in_bucket(self, bucket="", okey=""):
+        res = self.client.get_object(Bucket=bucket, Key=okey)
+        print(">>> geting object %s: ContentLength=%s, ContentType=%s"%(okey, res['ContentLength'], res['ContentType']))
 
-    def remove_readonly_to(self, uuid="", bucket=""):
-        if uuid != "" and bucket != "":
-            existing_ids = self._parse_id_in_acl(
-                self.get_bucket_acl(bucket)['Grants'])
-
-            exclu_ids = [x for x in existing_ids if x != uuid]
-            grant_read = ",".join(["id=\"%s\"" % x for x in exclu_ids])
-            print(grant_read)
-            self.client.put_bucket_acl(
-                Bucket=testing_bkt,
-                GrantRead=grant_read,
-            )
+    def remove_bucket_policy(self, bucket=""):
+        if bucket != "":
+            self.client.delete_bucket_policy(Bucket=bucket)
 
 
 s3_user1 = cos_client(whom='user1')
@@ -87,13 +92,16 @@ print("user1 check %s" % testing_bkt,
       s3_user1.list_objects_by_bucket(bucket=testing_bkt))
 
 s3_user2 = cos_client(whom='user2')
+print(s3_user2.get_uid())
 
 s3_user1.share_readonly_to(uuid=s3_user2.get_uid(), bucket=testing_bkt)
 
 print("user2 check %s" % testing_bkt,
       s3_user2.list_objects_by_bucket(bucket=testing_bkt))
 
-s3_user1.remove_readonly_to(uuid=s3_user2.get_uid(), bucket=testing_bkt)
+s3_user2.get_obj_in_bucket(bucket=testing_bkt, okey=testing_okey)
+
+s3_user1.remove_bucket_policy(bucket=testing_bkt)
 
 print("user2 check %s (no permission)" % testing_bkt)
 print(s3_user2.list_objects_by_bucket(bucket=testing_bkt))
